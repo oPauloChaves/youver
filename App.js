7cd5fd780f001c2306ebb1c933c15aeb4ff57696
Paulo Chaves <opaulochaves@gmail.com>
2017-10-09 08:53:06 -0300

    add native-base

--- App.js
+++ App.js
@@ -1,46 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, {Component} from "react"
-import {Platform, StyleSheet, Text, View} from "react-native"
+// import {View, Text} from "react-native"
+import App from "./src/App"
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
-})
+export default class AppRoot extends Component {
+  // state = {
+  //   isReady: false,
+  // }
 
-export default class App extends Component<{}> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    )
+    // if (!this.state.isReady) {
+    //   return (
+    //     <View>
+    //       <Text>Loading...</Text>
+    //     </View>
+    //   )
+    // }
+    return <App />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-  },
-})
